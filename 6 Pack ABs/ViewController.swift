67bbec4311e1b89376de7369377b9aa89d002573
Raymond Chau <guinextwo@gmail.com>
2019-01-09 08:25:22 -0800

    Initial Commit

---
+++ 6 Pack ABs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  6 Pack ABs
+//
+//  Created by Raymond Chau on 1/9/19.
+//  Copyright © 2019 Egg Altar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
